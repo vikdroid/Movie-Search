c39eba11a12323fc38a12079bad31eca2e5fda3e
Vikram Raj <vikram@LivingRoom-NoteBook.(none)>
2014-08-27 16:44:37 +0530

    show movie poster also in movie detail

--- js/movie.js
+++ js/movie.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 		  		$('#loading ul').addClass('ul');
 		  		for(var i=0; i<data.Search.length; i++)
 		  		{
-		  			$('#loading ul').append('<li><span>Title: </span><span>'+data.Search[i].Title+'</span><br><span>Year: </span>'+data.Search[i].Year+'<br><span>Type: </span>'+data.Search[i].Type+'</li>');
+		  			$('#loading ul').append('<li><span>Title: </span><span>'+data.Search[i].Title+'</span><br><span>Year: </span><span>'+data.Search[i].Year+'</span><br><span>Type: </span>'+data.Search[i].Type+'</li>');
 		  		}
 		  	}
 		})
@@ -25,15 +25,17 @@ $(document).ready(function(){
 
 	$(document).delegate('#loading ul li', 'click', function(){
 		var title = $(this).find('span:eq(1)').text();
+		var year = $(this).find('span:eq(3)').text();
 		//console.log(title);
 		$.ajax({
-			url: "http://www.omdbapi.com/?t="+title,
+			url: "http://www.omdbapi.com/?t="+title+"&y="+year,
 			type: "GET",
 			crossDomain: true,
 			dataType: 'jsonp',
 			success: function(data) {
 				$('#loading').html('');
 				console.log(data.Title);
+				$('#loading').append('<img alt="Poster Not Available" src='+data.Poster+'><br>');
 				$('#loading').append('<span>Title: </span>'+data.Title+'<br><span>Year: </span>'+data.Year+'<br><span>ImdbRating: </span>'+data.imdbRating+'<br><span>Release Date: </span>'+data.Released+'<br><span>Runtime: </span>'+data.Runtime+'<br><span>Genre: </span>'+data.Genre+'<br><span>Director: </span>'+data.Director+'<br><span>Writer: </span>'+data.Writer+'<br><span>Actors: </span>'+data.Actors+'<br><span>Story: </span>'+data.Plot+'<br><span>Language: </span>'+data.Language+'<br><span>Country: </span>'+data.Country+'<br><span>Awards: </span>'+data.Awards+'<br><span>imdbVotes: </span>'+data.imdbVotes+'<br><span>Type: </span>'+data.Type+'<br>');
 			}
 		});
